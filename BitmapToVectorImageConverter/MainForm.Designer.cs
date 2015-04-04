deb3b8111c5e8c32135dd46056af6210d6f1b142
Mateusz Jankowski <janektbg@gmail.com>
2015-04-04 16:46:43 +0200

    initial winforms project with some GUI done

---
+++ BitmapToVectorImageConverter/MainForm.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace BitmapToVectorImageConverter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputPictureBox = new System.Windows.Forms.PictureBox();
+            this.MenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openInputFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openVectorImageForComparisonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openInputImageDialog = new System.Windows.Forms.OpenFileDialog();
+            this.inputPictureBoxLabel = new System.Windows.Forms.Label();
+            this.algorithmPropertiesGroupBox = new System.Windows.Forms.GroupBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.outputPictureBox = new System.Windows.Forms.PictureBox();
+            this.outputPictureBoxLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.vectorImageForComparisonPictureBoxLabel = new System.Windows.Forms.Label();
+            this.vectorImageForComparisonPictureBox = new System.Windows.Forms.PictureBox();
+            this.openVectorImageForComparisonFileDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.inputPictureBox)).BeginInit();
+            this.MenuStrip.SuspendLayout();
+            this.algorithmPropertiesGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.outputPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vectorImageForComparisonPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputPictureBox
+            // 
+            this.inputPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.inputPictureBox.Location = new System.Drawing.Point(12, 57);
+            this.inputPictureBox.Name = "inputPictureBox";
+            this.inputPictureBox.Size = new System.Drawing.Size(300, 300);
+            this.inputPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.inputPictureBox.TabIndex = 0;
+            this.inputPictureBox.TabStop = false;
+            this.inputPictureBox.Visible = false;
+            // 
+            // MenuStrip
+            // 
+            this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.MenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip.Name = "MenuStrip";
+            this.MenuStrip.Size = new System.Drawing.Size(984, 24);
+            this.MenuStrip.TabIndex = 1;
+            this.MenuStrip.Text = "MenuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openInputFileToolStripMenuItem,
+            this.openVectorImageForComparisonToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openInputFileToolStripMenuItem
+            // 
+            this.openInputFileToolStripMenuItem.Name = "openInputFileToolStripMenuItem";
+            this.openInputFileToolStripMenuItem.Size = new System.Drawing.Size(259, 22);
+            this.openInputFileToolStripMenuItem.Text = "Open input raster image";
+            this.openInputFileToolStripMenuItem.Click += new System.EventHandler(this.openInputFileToolStripMenuItem_Click);
+            // 
+            // openVectorImageForComparisonToolStripMenuItem
+            // 
+            this.openVectorImageForComparisonToolStripMenuItem.Name = "openVectorImageForComparisonToolStripMenuItem";
+            this.openVectorImageForComparisonToolStripMenuItem.Size = new System.Drawing.Size(259, 22);
+            this.openVectorImageForComparisonToolStripMenuItem.Text = "Open vector image for comparison";
+            this.openVectorImageForComparisonToolStripMenuItem.Click += new System.EventHandler(this.openVectorImageForComparisonToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(256, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(259, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openInputImageDialog
+            // 
+            this.openInputImageDialog.Filter = "BMP files (*.bmp)|*.bmp";
+            this.openInputImageDialog.Title = "Open input raster image";
+            // 
+            // inputPictureBoxLabel
+            // 
+            this.inputPictureBoxLabel.AutoSize = true;
+            this.inputPictureBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.inputPictureBoxLabel.ForeColor = System.Drawing.Color.Red;
+            this.inputPictureBoxLabel.Location = new System.Drawing.Point(77, 34);
+            this.inputPictureBoxLabel.Name = "inputPictureBoxLabel";
+            this.inputPictureBoxLabel.Size = new System.Drawing.Size(171, 20);
+            this.inputPictureBoxLabel.TabIndex = 2;
+            this.inputPictureBoxLabel.Text = "No input image loaded!";
+            // 
+            // algorithmPropertiesGroupBox
+            // 
+            this.algorithmPropertiesGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.algorithmPropertiesGroupBox.Controls.Add(this.convertButton);
+            this.algorithmPropertiesGroupBox.Location = new System.Drawing.Point(12, 549);
+            this.algorithmPropertiesGroupBox.Name = "algorithmPropertiesGroupBox";
+            this.algorithmPropertiesGroupBox.Size = new System.Drawing.Size(960, 100);
+            this.algorithmPropertiesGroupBox.TabIndex = 3;
+            this.algorithmPropertiesGroupBox.TabStop = false;
+            this.algorithmPropertiesGroupBox.Text = "Algorithm properties";
+            // 
+            // convertButton
+            // 
+            this.convertButton.Enabled = false;
+            this.convertButton.Location = new System.Drawing.Point(443, 71);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 0;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // outputPictureBox
+            // 
+            this.outputPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputPictureBox.Location = new System.Drawing.Point(672, 57);
+            this.outputPictureBox.Name = "outputPictureBox";
+            this.outputPictureBox.Size = new System.Drawing.Size(300, 300);
+            this.outputPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.outputPictureBox.TabIndex = 4;
+            this.outputPictureBox.TabStop = false;
+            this.outputPictureBox.Visible = false;
+            // 
+            // outputPictureBoxLabel
+            // 
+            this.outputPictureBoxLabel.AutoSize = true;
+            this.outputPictureBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.outputPictureBoxLabel.ForeColor = System.Drawing.Color.Red;
+            this.outputPictureBoxLabel.Location = new System.Drawing.Point(719, 34);
+            this.outputPictureBoxLabel.Name = "outputPictureBoxLabel";
+            this.outputPictureBoxLabel.Size = new System.Drawing.Size(207, 20);
+            this.outputPictureBoxLabel.TabIndex = 5;
+            this.outputPictureBoxLabel.Text = "No output image generated!";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 363);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(960, 180);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Image comparison statistics";
+            // 
+            // vectorImageForComparisonPictureBoxLabel
+            // 
+            this.vectorImageForComparisonPictureBoxLabel.AutoSize = true;
+            this.vectorImageForComparisonPictureBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.vectorImageForComparisonPictureBoxLabel.ForeColor = System.Drawing.Color.Red;
+            this.vectorImageForComparisonPictureBoxLabel.Location = new System.Drawing.Point(348, 34);
+            this.vectorImageForComparisonPictureBoxLabel.Name = "vectorImageForComparisonPictureBoxLabel";
+            this.vectorImageForComparisonPictureBoxLabel.Size = new System.Drawing.Size(288, 20);
+            this.vectorImageForComparisonPictureBoxLabel.TabIndex = 8;
+            this.vectorImageForComparisonPictureBoxLabel.Text = "No vector image for comparison loaded!";
+            // 
+            // vectorImageForComparisonPictureBox
+            // 
+            this.vectorImageForComparisonPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.vectorImageForComparisonPictureBox.Location = new System.Drawing.Point(342, 57);
+            this.vectorImageForComparisonPictureBox.Name = "vectorImageForComparisonPictureBox";
+            this.vectorImageForComparisonPictureBox.Size = new System.Drawing.Size(300, 300);
+            this.vectorImageForComparisonPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.vectorImageForComparisonPictureBox.TabIndex = 7;
+            this.vectorImageForComparisonPictureBox.TabStop = false;
+            this.vectorImageForComparisonPictureBox.Visible = false;
+            // 
+            // openVectorImageForComparisonFileDialog
+            // 
+            this.openVectorImageForComparisonFileDialog.Title = "Open vector image for comparison";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.algorithmPropertiesGroupBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.vectorImageForComparisonPictureBoxLabel);
+            this.Controls.Add(this.vectorImageForComparisonPictureBox);
+            this.Controls.Add(this.outputPictureBoxLabel);
+            this.Controls.Add(this.outputPictureBox);
+            this.Controls.Add(this.inputPictureBoxLabel);
+            this.Controls.Add(this.inputPictureBox);
+            this.Controls.Add(this.MenuStrip);
+            this.MinimumSize = new System.Drawing.Size(1000, 700);
+            this.Name = "MainForm";
+            this.Text = "Bitmap To Vector Image Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.inputPictureBox)).EndInit();
+            this.MenuStrip.ResumeLayout(false);
+            this.MenuStrip.PerformLayout();
+            this.algorithmPropertiesGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.outputPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vectorImageForComparisonPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox inputPictureBox;
+        private System.Windows.Forms.MenuStrip MenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openInputImageDialog;
+        private System.Windows.Forms.ToolStripMenuItem openInputFileToolStripMenuItem;
+        private System.Windows.Forms.Label inputPictureBoxLabel;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.GroupBox algorithmPropertiesGroupBox;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.PictureBox outputPictureBox;
+        private System.Windows.Forms.Label outputPictureBoxLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label vectorImageForComparisonPictureBoxLabel;
+        private System.Windows.Forms.PictureBox vectorImageForComparisonPictureBox;
+        private System.Windows.Forms.ToolStripMenuItem openVectorImageForComparisonToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openVectorImageForComparisonFileDialog;
+    }
+}
+
---
+++ BitmapToVectorImageConverter/MainForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitmapToVectorImageConverter
+{
+    public partial class MainForm : Form
+    {
+        private Bitmap _inputImage;
+        private Bitmap _vectorImageForComparison;
+        private Bitmap _outputImage;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openInputFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openInputImageDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    _inputImage = new Bitmap(openInputImageDialog.FileName);
+                }
+                catch (Exception ex)
+                {
+                    ShowError(ex);
+                }
+            }
+            if (_inputImage != null)
+            {
+                inputPictureBox.Image = _inputImage;
+                inputPictureBox.Visible = true;
+                inputPictureBoxLabel.Text = "Input image";
+                inputPictureBoxLabel.ForeColor = Color.Black;
+                convertButton.Enabled = true;
+            }
+            else
+            {
+                inputPictureBox.Visible = false;
+                inputPictureBoxLabel.Text = "No input image loaded!";
+                inputPictureBoxLabel.ForeColor = Color.Red;
+                convertButton.Enabled = false;
+            }
+        }
+
+        private void openVectorImageForComparisonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openVectorImageForComparisonFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    _vectorImageForComparison = new Bitmap(openVectorImageForComparisonFileDialog.FileName);
+                }
+                catch (Exception ex)
+                {
+                    ShowError(ex);
+                }
+            }
+            if (_vectorImageForComparison != null)
+            {
+                vectorImageForComparisonPictureBox.Image = _vectorImageForComparison;
+                vectorImageForComparisonPictureBox.Visible = true;
+                vectorImageForComparisonPictureBoxLabel.Text = "Vector image for comparison";
+                vectorImageForComparisonPictureBoxLabel.ForeColor = Color.Black;
+            }
+            else
+            {
+                vectorImageForComparisonPictureBox.Visible = false;
+                vectorImageForComparisonPictureBoxLabel.Text = "No vector image for comparison loaded!";
+                vectorImageForComparisonPictureBoxLabel.ForeColor = Color.Red;
+            }
+        }
+
+        private void ShowError(Exception ex)
+        {
+            const string title = "Error";
+            String error = "An error occured while loading file. Please try again.\nError: " + ex.Message;
+            MessageBox.Show(error, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            if (_inputImage != null)
+            {
+                //TODO start konwersji obrazka z _inputImage z zadanymi parametrami
+                /* 
+                 * otrzymujemy _outputImage
+                 * aktualnie przepisuje _inputImage do _outputImage bez zmian
+                 */
+                _outputImage = new Bitmap(_inputImage);
+                if (_outputImage != null)
+                {
+                    outputPictureBox.Image = _inputImage;
+                    outputPictureBox.Visible = true;
+                    outputPictureBoxLabel.Text = "Output image";
+                    outputPictureBoxLabel.ForeColor = Color.Black;
+                }
+                else
+                {
+                    outputPictureBox.Visible = false;
+                    outputPictureBoxLabel.Text = "No output image generated!";
+                    outputPictureBoxLabel.ForeColor = Color.Red;
+                }
+            }
+        }
+    }
+}
