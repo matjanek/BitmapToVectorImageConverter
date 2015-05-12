793728388f23a4b216dc4d868fecaef015024e2d
Tomasz Półgrabia <tpsa.s.a@gmail.com>
2015-05-12 21:21:40 +0200

    Testy.

--- BitmapToVectorImageConverter/r2v/SvgTools.cs
+++ BitmapToVectorImageConverter/r2v/SvgTools.cs
@@ -14,21 +14,34 @@ namespace BitmapToVectorImageConverter
 		static public void save(List<GisPolygonR2V> polygons, string path) {
 			var doc = new SvgDocument ();
 
+			var g = new SvgGroup ();
+
+			Console.WriteLine ("Liczba wielokatow: {0}", polygons.Count);
+
 			foreach (var polygon in polygons) {
-				var poly = new SvgPolygon ();
+				Console.WriteLine ("Wielokat: {0}, dl: {1}", polygon.c, polygon.mBorderPoints.Count);
+				var poly = new SvgPolygon () {
+					Stroke = new SvgColourServer(Color.Red),
+					Fill = new SvgColourServer(Color.Black),
+					StrokeWidth = 1
+				};
+
 				poly.Points = new SvgPointCollection ();
 				var points = polygon.mBorderPoints;
 				// FIXME czy może połączyć left i right skiny?
 
 				foreach (var point in points) {
+					Console.WriteLine ("Punkty: ({0},{1})", point.X, point.Y);
 					poly.Points.Add (new SvgUnit (point.X));
 					poly.Points.Add (new SvgUnit (point.Y));
+					doc.Width  = Math.Max (doc.Width, point.X);
+					doc.Height = Math.Max (doc.Height, point.Y);
 				}
 
-				doc.Children.Add (poly);
+				g.Children.Add (poly);
 
 			}
-	
+			doc.Children.Add (g);
 			doc.Write (path);
 		}
 	}
